3355d8e76baaf30b8b904a8836bbc79ffae13139
ronak <ronaksingh2708@gmail.com>
2024-12-07 16:05:24 +0530

    JWT tokens created and authenticated

--- GO/http_server/main.go
+++ GO/http_server/main.go
@@ -3,8 +3,53 @@ package main
 import (
 	"fmt"
 	"html"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
+
+func genJWT() string {
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte("secret"))
+
+	fmt.Println(tokenString, err)
+
+	return tokenString
+}
+
+func checkJWT(tokenString string) {
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	// useful if you use multiple keys for your application.  The standard is to use 'kid' in the
+	// head of the token to identify which key to use, but the parsed token (head and claims) is provided
+	// to the callback, providing flexibility.
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		fmt.Println(err)
+	}
+}
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
@@ -16,6 +61,8 @@ func home(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	checkJWT(genJWT())
 
 	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8090", nil)
